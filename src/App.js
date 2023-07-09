0a5f1aa96b068002e98077dccd4bb7277eb563fc
Astghik Simonyan <astghiksimonyan56@gmail.com>
2023-07-09 12:29:19 +0400

    Adding Basic Text Fields

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import * as React from 'react';
+import BasicTextFields from "./Components/BasicTextFields/BasicTextFields";
 
 function App() {
   return <div className="App">
+    <BasicTextFields/>
     </div>
 }
 
