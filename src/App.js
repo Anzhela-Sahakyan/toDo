f2ef57d13313260dd09ebd7845028af2039affa7
Anzhela <sahakyananzhela@gmail.com>
2023-07-09 12:08:51 +0400

    feat: adding an nput box

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Todo from "./components/ToDo/Todo";
 
 function App() {
-  return <div className="App">Hello guys</div>;
+  return (
+    <div className="App">
+      <Todo />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ToDo/Todo.jsx
@@ -0,0 +1,5 @@
+import { TextField } from "@mui/material";
+
+export default function Todo({ text }) {
+  return <TextField id="outlined-basic" label="Outlined" variant="outlined" />;
+}
